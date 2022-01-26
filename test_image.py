f2a501306103ecf226d30b3d1ab9b67dfbdda197
Shakhrul Iman Siam <61387093+ShakhrulSiam268@users.noreply.github.com>
2022-01-26 13:12:32 +0600

    webcam detection

--- test_image.py
+++ test_image.py
@@ -3,17 +3,16 @@ from detector import DetectYolov5
 import time
 
 source = 'random.jpeg'
+#source = '0.jpg'
 image = cv2.imread(source)
 
-model_path = './model_data/yolov5s.pt'
+model_path = './model_data/yolov5m.pt'
+#model_path = 'C:/Users/User/Desktop/ACI/yolov5_models/crowd/crowdhuman_yolov5m_deepakcrk.pt'
 detector = DetectYolov5(model_path)
 
 t1 = time.time()
-
-img, bboxes, names = detector.detect_image(image, conf_threshold = 0.3)
-
+img, bboxes, names = detector.detect_image(image, conf_threshold = 0.2)
 t2 = time.time()
-
 print('Processing Time : {}'.format(round(t2-t1,3)))
 
 cv2.imshow('Frame',img)
--- webcam_detect.py
+++ webcam_detect.py
@@ -6,19 +6,19 @@ from utils.videocapture import CustomVideoCapture
 
 model_path = './model_data/yolov5l.pt'
 detector = DetectYolov5(model_path)
+url = 0          # use Url for RTSP Stream 
 
-url = 'rtsp://admin:AiBi@8899@192.168.101.80:554'
 cap = CustomVideoCapture(url)
 
 while True:
     t1 = time.time()
     frame = cap.read()
-    image, bboxes, names = detector.detect_image(frame, conf_threshold = 0.6, resize_factor = 0.4)
-    #image = cv2.resize(img,(640,640))
+    image, bboxes, names = detector.detect_image(frame, conf_threshold = 0.2, resize_factor = 0.7)
+    image = cv2.resize(image,(640,640))
     t2 = time.time()
     print('Processing Time : {} s'.format(round((t2-t1),3)))
 
-    cv2.imshow('IP Camera Object Detection',image)
+    cv2.imshow('Webcam Object Detection',image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break  
 
